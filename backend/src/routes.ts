181341f877b55ac8f79d74f5e4e3b28e4fd98323
Corey Patrick <cpatri2@pdx.edu>
2023-05-02 18:55:48 -0400

    *Adds User CRUD Feature *Adds Docker/Compose setup for backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,42 +16,67 @@ async function DoggrRoutes(app:FastifyInstance, _options = {}) {
 		return req.em.find(User, {});
 	});
 
-	app.route<{Body:{email:string}}>({
-		method: "SEARCH",
-		url: "/users",
-
-		handler: async (req, reply) => {
-			const {email} = req.body;
-			try {
-				const theUser =req.em.find(User, {email});
-				console.log(theUser);
-				reply.send(theUser);
-			} catch (err){
-				console.log(err);
-				reply.status(500).send(err);
-			}
-		}
-	});
-	
 	app.post<{Body: ICreateUsersBody}>("/users", async (req, reply) => {
 		const { name, email, petType} = req.body;
-		
+
 		try {
 			const newUser = await req.em.create(User, {
 				name,
 				email,
 				petType
 			});
-			
+
 			await req.em.flush();
-			
+
 			console.log("Created new user:", newUser);
 			return reply.send(newUser);
 		} catch (err) {
 			console.log("Failed to create new user", err.message);
 			return reply.status(500).send({message: err.message});
 		}
-		
+
 	});
+
+	//READ
+	app.search<{Body: { email: string}}>("/users", async (req, reply) => {
+		const {email} = req.body;
+
+		try {
+			const theUser = await req.em.findOne(User, {email});
+			console.log(theUser);
+			reply.send(theUser);
+		} catch (err) {
+			console.error(err);
+			reply.status(500)
+				.send(err);
+		}
+	});
+
+	//UPDATE
+	app.put<{Body: {email:string, name:string, petType:string}}>("/users", async (req, reply) =>{
+		const {email, name, petType} = req.body;
+
+		const userToChange = await req.em.findOne(User, {email});
+		userToChange.name = name;
+		userToChange.petType = petType;
+
+		// This is how we persist our JS object changes to the database itself
+		await req.em.flush();
+		console.log(userToChange);
+		reply.send(userToChange);
+	});
+
+	// D
+	app.delete<{Body: { email: string }}>("/users", async(req, reply) => {
+		const {email} = req.body;
+
+		// using reference is enough, no need for a fully initialized entity
+		const userToDelete = await req.em.findOne(User, {email});
+
+		await req.em.remove(userToDelete).flush();
+		reply.send();
+
+	});
+
 }
 export default DoggrRoutes;
